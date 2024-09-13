14ed63a8d85c478769b42b70fd4b96e7c67619d2
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2024-09-13 12:36:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
         markdownPreview.innerHTML = htmlContent;
     }
 
+    document.getElementById("selectMarkdown").addEventListener("click", function () {
+        document.getElementById("fileInput").click();
+    });
+
+    document.getElementById("fileInput").addEventListener("change", function (event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                const content = e.target.result;
+                document.getElementById("markdown-input").value = content;
+                updatePreview(content);
+            };
+            reader.readAsText(file);
+        }
+    });
+
     markdownInput.addEventListener('input', updatePreview);
 
     exportHtmlButton.addEventListener('click', () => {
@@ -33,5 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
         doc.text(markdownPreview.innerText, 10, 10);
         doc.save('document.pdf');
     });
+
     updatePreview();
 });
